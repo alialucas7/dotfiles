1e36ac3350598946286970265ca5b7d25f0d0152
alialucas7 <lukitis2@gmail.com>
2025-07-17 11:02:05 -0300

    update

--- dwm-ipc/config.h
+++ dwm-ipc/config.h
@@ -1,21 +1,17 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 5;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Ubuntu Mono:pixelsize=25:antialias=true:autohint=true" };
 
-//nuevas lineas luc4as
-static const int usealtbar	= 1;
-static const char *altbarclass	= "Polybar";
-static const char *altbarcmd	="$HOME/bar.sh";
-
-//
 
 
-
+static const int usealtbar = 1;
+static const char *altbarclass = "Polybar";
+static const char *altbarcmd = "$HOME/bar.sh";
 
 static const char dmenufont[]       = "monospace:size=18";
 static const char col_gray1[]       = "#222222";
@@ -27,14 +23,14 @@ static const char col_negro[]       = "#191919";
 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_negro },
+	[SchemeNorm] = { col_gray3, col_gray1, col_cyan },
 	[SchemeSel]  = { col_gray4, col_gray1,  col_negro  },
 };
 
 /* tagging */
 //static const char *tags[] = { " ", "2", " ", "4", "5", "6", " ", "8", " " };
 
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8"};
+static const char *tags[] = { "1", "2", "3", "4","5","6","7","8" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -87,20 +83,16 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 //static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *chrome[]   = {"firefox", NULL};
-static const char *thunare[]   = {"alacritty","-e","ranger",NULL};
+static const char *thunare[]   = {"alacritty","-e","spf",NULL};
 static const char *dmenucmd[] = {"rofi", "-show", "run" ,NULL};
-static const char *alacritty[] = {"alacritty", NULL};
-
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,			XK_n,	   spawn,	   {.v = thunare } },
-	{ MODKEY,			XK_s,	   spawn,	   SHCMD("sudo systemctl suspend") },
+	{ MODKEY,			XK_s,	   spawn,	   SHCMD("~/.switch_audio.sh") },
 	{ MODKEY,			XK_o,	   spawn,	   {.v = chrome } },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY|ShiftMask,		XK_l,	   spawn,	   {.v = alacritty} },
-	{ MODKEY,			XK_c,	   spawn,	   SHCMD("exec ~/.scripts/screenshot") },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -154,6 +146,8 @@ static Button buttons[] = {
 
 
 
+
+
 static const char *ipcsockpath = "/tmp/dwm.sock";
 static IPCCommand ipccommands[] = {
    IPCCOMMAND(  view,                1,      {ARG_TYPE_UINT}   ),
@@ -171,4 +165,3 @@ static IPCCommand ipccommands[] = {
    IPCCOMMAND(  setlayoutsafe,       1,      {ARG_TYPE_PTR}    ),
    IPCCOMMAND(  quit,                1,      {ARG_TYPE_NONE}   )
  };
-
